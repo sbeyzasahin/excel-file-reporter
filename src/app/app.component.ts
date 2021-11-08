d3d27250e5f1b87a3223acf07bbc03d06c09c897
sbeyzasahin <sumeyyebeyzasahin>
2021-11-08 22:58:07 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,22 @@ import { Component } from '@angular/core';
 import * as XLSX from 'xlsx';
 import * as Highcharts from 'highcharts';
 import highcharts3D from 'highcharts/highcharts-3d.src';
+import * as Provinces from 'src/assets/iller';
 // @ts-ignore
 highcharts3D(Highcharts);
+
+interface IAnswer {
+  name: string,
+  value: number
+}
+
+interface IData {
+  name: string,
+  y: number,
+  sliced?: boolean,
+  selected?: boolean,
+  color?: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,19 +27,51 @@ export class AppComponent {
   Highcharts: typeof Highcharts = Highcharts;
   chart!: Highcharts.Chart;
 
-  answerList: string[] = ['Evet / Hayır', 'Numerik Cevaplar'];
-  selectedAnswer: string = '-';
+  answerList: IAnswer[] = [
+    {
+      name: 'Evet / Hayır Cevaplar',
+      value: 1
+    },
+    // {
+    //   name: 'Numerik Cevaplar',
+    //   value: 2
+    // },
+    {
+      name: 'Kategori Seçiniz..',
+      value: 0
+    }];
 
+  selectedAnswer: IAnswer = { name: '-', value: 0 };
+
+  questionList: string[] = [];
+
+  dataSource: any[] = [];
+
+  selectedQuestion = '';
+
+  yesAnswerCount = 0;
+  noAnswerCount = 0;
 
   name = 'This is XLSX TO JSON CONVERTER';
   willDownload = false;
+
+  tableDataSource = [];
+  tableHeaderList: string[] = [];
+  provinceList = Provinces;
+
   getPieChartOptions = (): Highcharts.Options => {
     return {
       chart: {
-        type: 'pie'
+        type: 'pie',
+        width: 600,
+        height: 400
       },
       title: {
-        text: 'title'
+        text: this.selectedQuestion,
+        style: {
+          fontSize: '21px',
+          fontFamily: 'Helvetica'
+        }
       },
       tooltip: {
         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
@@ -41,12 +87,22 @@ export class AppComponent {
           cursor: 'pointer',
           dataLabels: {
             enabled: true,
-            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-          }
-        }
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+            style: {
+              fontSize: '21px',
+              fontFamily: 'Helvetica'
+            }
+          },
+
+        },
+
       },
+      credits: {
+        enabled: false
+      }
     }
   }
+  fileName: any;
 
   onSelectFile(event: any) {
 
@@ -54,13 +110,13 @@ export class AppComponent {
     let jsonData = null;
     const reader = new FileReader();
     const file = event.target.files[0];
-    console.log('file geldi.: ', file);
-
+    // console.log('file geldi.: ', file);
+    this.fileName = file.name
 
     reader.onload = (event) => {
-      const data = reader.result;
+      const result = reader.result;
 
-      workBook = XLSX.read(data, { type: 'binary' });
+      workBook = XLSX.read(result, { type: 'binary' });
 
       jsonData = workBook.SheetNames.reduce((initial: any, name: string) => {
         const sheet = workBook.Sheets[name];
@@ -68,20 +124,112 @@ export class AppComponent {
         return initial;
       }, {});
       const dataString = JSON.stringify(jsonData, null, 2);
-      console.log(dataString);
+      // console.log(dataString);
+
+      if (jsonData['Form Responses']) {
+        this.dataSource = jsonData['Form Responses'];
+      }
     }
     reader.readAsBinaryString(file);
 
+  }
 
-    // const chartOption = this.getPieChartOptions()
-    // chartOption.series = []
+  drawChart() {
 
-    // setTimeout(() => {
+    let data: IData[];
 
-    //   this.chart = Highcharts.chart('graph-1', chartOption);
-    // });
+    if (this.selectedAnswer.value === 1) {
+      data = [
+        {
+          name: 'Evet',
+          y: this.yesAnswerCount,
+          color: '#673ab7'
+
+        },
+        {
+          name: 'Hayır',
+          y: this.noAnswerCount,
+          color: '#e5d6ff'
+        },
+      ];
+
+      const chartOption = this.getPieChartOptions();
+
+      chartOption.series = [
+        {
+          name: this.selectedQuestion,
+          data: data
+        }
+      ] as Array<Highcharts.SeriesPieOptions>;;
+
+      setTimeout(() => {
+        this.chart = Highcharts.chart('graph-1', chartOption)
+      });
+
+    }
+
+
 
   }
 
+  createQuestions() {
+    this.questionList = [];
+
+    if (this.dataSource.length > 0) {
+
+      console.log('DATAAAAA: ', this.dataSource);
+
+
+      if (this.selectedAnswer.value === 1) {
+
+        const obj = this.dataSource[0];
+        for (const [key, value] of Object.entries(obj)) {
+
+          if (value === 'Evet' || value === 'Hayır') {
+            this.questionList.push(key);
+          }
+        }
+      } else if (this.selectedAnswer.value === 2) {
+
+        const obj = this.dataSource[0];
+        for (const [key, value] of Object.entries(obj)) {
+
+          if (typeof value === 'number') {
+            this.questionList.push(key);
+          }
+        }
+      }
+
+      console.log('question list: ', this.questionList);
+
+    } else {
+      console.log('DOSYADA VERİ BULUNAMADI!')
+    }
+
+  }
+
+  onSelectedQuestion() {
+
+    this.yesAnswerCount = 0;
+    this.noAnswerCount = 0;
+
+    this.dataSource.forEach((element) => {
+      if (this.selectedAnswer.value === 1) {
+        if (element[this.selectedQuestion] === 'Evet') {
+          this.yesAnswerCount++;
+
+        } else if (element[this.selectedQuestion] === 'Hayır') {
+          this.noAnswerCount++;
+        }
+
+      }
+    });
+
+    this.drawChart();
+  }
+
+  getYesOrNotAnswersOfProvinceList() {
+    this.tableHeaderList = ['yes', 'no'];
+  }
 
 }
